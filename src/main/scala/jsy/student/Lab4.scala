f8c746d8fe0129cc7292b8daddab2ce72ef4e24f
Andrew Casner <a.casner@comcast.net>
2018-03-08 20:50:36 -0700

    typeof case BOP Minus

--- src/main/scala/jsy/student/Lab4.scala
+++ src/main/scala/jsy/student/Lab4.scala
@@ -122,8 +122,12 @@ object Lab4 extends jsy.util.JsyApplication with Lab4Like {
           case (tgot, _) => err(tgot, e1)
           case (_, tgot) => err(tgot, e2)
         }
-      case Binary(Minus|Times|Div, e1, e2) => 
-        ???
+      case Binary(Minus|Times|Div, e1, e2) =>
+        (typeof(env, e1), typeof(env, e2)) match {
+          case (TNumber, TNumber) => TNumber
+          case (tgot, _) => err(tgot, e1)
+          case (_, tgot) => err(tgot, e2)
+        }
       case Binary(Eq|Ne, e1, e2) =>
         ???
       case Binary(Lt|Le|Gt|Ge, e1, e2) =>
